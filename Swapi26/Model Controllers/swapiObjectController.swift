de877b4101731993a75be14dd7f5b0f660e6169d
Cody <codyadcock10@gmail.com>
2019-05-16 11:52:59 -0600

    Starting Point

---
+++ Swapi26/Model Controllers/swapiObjectController.swift
@@ -0,0 +1,9 @@
+//
+//  swapiObjectController.swift
+//  Swapi26
+//
+//  Created by Cody on 5/16/19.
+//  Copyright © 2019 Cody Adcock. All rights reserved.
+//
+
+import Foundation
---
+++ Swapi26/Models/swapiObject.swift
@@ -0,0 +1,9 @@
+//
+//  swapiObject.swift
+//  Swapi26
+//
+//  Created by Cody on 5/16/19.
+//  Copyright © 2019 Cody Adcock. All rights reserved.
+//
+
+import Foundation
