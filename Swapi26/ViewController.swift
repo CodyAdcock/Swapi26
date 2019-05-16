74d1e08363659fe0c8ac9e25576ebdb7096ef743
Cody <codyadcock10@gmail.com>
2019-05-16 11:17:50 -0600

    First Commit

---
+++ Swapi26/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swapi26
+//
+//  Created by Cody on 5/16/19.
+//  Copyright © 2019 Cody Adcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
